78f35c15d9bdd6fa573ff3ef5acbf0dcf400eabc
liuweilin <2207284210@qq.com>
2019-07-06 22:16:14 +0800

    demo commit

--- src/main/java/com/ups/controller/LoginController.java
+++ src/main/java/com/ups/controller/LoginController.java
@@ -1,4 +1,14 @@
 package com.ups.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/")
 public class LoginController {
+
+    @RequestMapping(value = "/login")
+    public String sayHello() {
+        return "Hello world!";
+    }
 }
